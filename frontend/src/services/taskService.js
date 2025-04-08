94a96c4361d56f8d955cd4e6c9c71c99684522cd
draganvukman <dvukman991@gmail.com>
2025-04-09 03:32:14 +0500

    frontend api url update

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authService from './authService';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
 
 const taskService = {
     getAllTasks: async () => {
